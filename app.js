03ad47390a518ecc85c577990c823406e2a5e176
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2021-02-07 05:18:51 +0600

    Update

---
+++ app.js
@@ -0,0 +1,75 @@
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
+
+function getMealList() {
+  let searchInputTxt = document.getElementById("search-input").value;
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInputTxt}`
+  )
+    .then((response) => response.json())
+
+    .then((data) => {
+      let html = "";
+      if (data.meals) {
+        const mealResult = document.getElementById("meal-result");
+        const mealList = document.getElementById("meal");
+        data.meals.forEach((meal) => {
+          html += `
+          <div onclick ="getMealByID('${meal.idMeal}')" class = "meal-item" data-id = "${meal.idMeal}">
+              <div class = "meal-img">
+                  <img src = "${meal.strMealThumb}" alt = "food">
+              </div>
+              <div class = "meal-name">
+                  <h3>${meal.strMeal}</h3>
+                  <a href = "#" class = "recipe-btn">Get Recipe</a>
+              </div>
+          </div>
+      `;
+          mealList.innerHTML = html;
+          console.log(html);
+        });
+      } else {
+        const mealList = document.getElementById("meal");
+        html = `<h1 style ="text-align:center">Sorry, we didn't find any meal!</h1>`;
+        mealList.innerHTML = html;
+      }
+
+      console.log(html);
+    });
+}
+
+function getMealByID(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const meal = data.meals[0];
+      getIngredientList(meal);
+    });
+}
+
+function getIngredientList(meal) {
+  const ingredients = [];
+  for (let i = 1; i < 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]} `
+      );
+    } else {
+      break;
+    }
+  }
+  mealDetailsContent.innerHTML = `
+  <div class="meal-info">
+  <h1>${meal.strMeal}</h1>
+  <img src="${meal.strMealThumb}" alt="food" />
+  <div class="meal-ingredient">
+    <h2>Ingredients</h2>
+    <ul>
+      ${ingredients.map((ing) => `<li> ${ing}</li>`).join()}
+    </ul>
+  </div>
+  </div>
+  
+  `;
+}
